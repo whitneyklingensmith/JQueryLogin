70fa4bbeaee3b225bafb9d21f07b68a31153d10b
Whitney Murray <wnksmith92@yahoo.com>
2015-12-28 17:04:01 -0600

    validation is still not working properly && still doesn't go to ironyard

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,29 +1,26 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 $(document).ready(function () {
-	var $registerForm = $('#registerForm');
-	$(registerForm).submit(function (e) {
+	var submit = $('#submit');
+	var registerForm = $('#registerForm');
+	registerForm.submit(function (e) {
 		e.preventDefault();
 		var email = $('#email').val();
 		var password = $('#pass').val();
 		var error = $('#error').html(' ');
 		var hasError = false;
 
-		if (email === 'aaron@theironyard.com' || email === 'admin@google.com') {
-			error.hide();
-			hasError = false;
-		} else if (password === 'password123' || password === 'honeycrisp' || password === 'pandas') {
-			error.hide();
-			hasError = false;
-		} else if (email !== 'aaron@theironyard.com' || email !== 'admin@google.com') {
+		if (email !== 'aaron@theironyard.com' || email !== 'admin@google.com') {
 			error.html('Trying to get in with a wrong username. Get outttttaaaa here');
-			hasError = true;
+			error.show();
 		} else if (password !== 'honeycrisp' || password !== 'pandas' || password !== 'password123') {
 			error.html('Your password is a fake');
-			hasError = true;
-		} else {
-			hasError = false;
-			window.location = 'http://www.theironyard.com';
+			error.show;
+		} else if ('input' === " ") {
+			error.html('Thought you could just not fill anything out ya dingus');
+		} else if (email === 'aaron@theironyard.com' && password === 'password123' || email === 'admin@google.com' && password === 'pandas' || email === 'admin@google.com' && password === 'honeycrisp') {
+			registerForm.hide();
+			window.location.replace = 'http://www.theironyard.com/home.html'.show();
 		}
 	});
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,31 @@
 'use strict';
 $(document).ready(function(){
-	var $registerForm = $('#registerForm');
-$(registerForm).submit(function(e){
+	var submit = $('#submit');
+	var registerForm = $('#registerForm');
+registerForm.submit(function(e){
 	e.preventDefault();
 	var email = $('#email').val();
 	var password = $('#pass').val();
 	var error = $('#error').html(' ');
 	var hasError = false;
 	
-	if(email === 'aaron@theironyard.com' || email === 'admin@google.com' ){
-		error.hide();
-		hasError = false;
-
-	}
-	else if(password === 'password123' || password === 'honeycrisp' || password === 'pandas'){
-		error.hide();
-		hasError = false;
-	}
-	else if(email !== 'aaron@theironyard.com' || email !== 'admin@google.com'){
+	
+	 if(email !== 'aaron@theironyard.com' || email !== 'admin@google.com'){
 		error.html('Trying to get in with a wrong username. Get outttttaaaa here');
-		hasError = true;
+		error.show();
+		
 	}
 	else if(password !== 'honeycrisp' || password !== 'pandas' || password !== 'password123'){
 		error.html('Your password is a fake');
-		hasError = true;
+		error.show;
+		
 	}
-	else{
-		hasError = false;
-		window.location = 'http://www.theironyard.com';
+	else if('input' === " "){
+		error.html('Thought you could just not fill anything out ya dingus');
+	}
+	else if(email === 'aaron@theironyard.com' && password === 'password123' || email === 'admin@google.com' && password === 'pandas' || email === 'admin@google.com' && password === 'honeycrisp'){
+		registerForm.hide();
+		window.location.replace = ('http://www.theironyard.com/home.html').show();
 	}
 
 	});
